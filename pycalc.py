ee36ff0de932c83c56475e0d5dc76e9f52f16fad
Official21A <officialamirhossein21@gmail.com>
2020-09-30 08:18:03 +0330

    Controller add

--- pycalc.py
+++ pycalc.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QVBoxLayout
 
+from functools import partial
+
 
 __version__ = '0.1'
 __author__ = 'Amirhossein'
@@ -86,15 +88,37 @@ class PyCalcUi(QMainWindow):
         self.setDisplayText('')
 
 
+class PyCalcCtrl: # this is the controller class
+    def __init__(self, view):
+        """Controller initializer."""
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
+
+    def _buildExpression(self, sub_exp):
+        """Build expression."""
+        expression = self._view.displayText() + sub_exp
+        self._view.setDisplayText(expression)
+
+    def _connectSignals(self):
+        """Connect signals and slots."""
+        for btnText, btn in self._view.buttons.items():
+            if btnText not in {'=', 'C'}:
+                btn.clicked.connect(partial(self._buildExpression, btnText))
+
+        self._view.buttons['C'].clicked.connect(self._view.clearDisplay)        
+
+
 
 def main():
-    """Main function."""
     # Create an instance of QApplication
     pycalc = QApplication(sys.argv)
     # Show the calculator's GUI
     view = PyCalcUi()
     view.show()
-    # Execute the calculator's main loop
+    # Create instances of the model and the controller
+    PyCalcCtrl(view=view)
+    # Execute calculator's main loop
     sys.exit(pycalc.exec_())
 
 
